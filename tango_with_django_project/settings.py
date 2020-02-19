b2c6d54f0d1df7aa9ab0a2ad3d3c5b3d110dac1f
Lyubomir Lazarov (student) <2072353L@student.gla.ac.uk>
2020-02-19 13:58:21 +0000

    chapter 9 finished

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -96,6 +96,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS' : { 'min_length': 6,}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -104,6 +105,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
+LOGIN_URL = 'rango:login'
 
 
 # Internationalization
